92fcaa37834ec6d206d9739a013350c202b16fa7
Roman Ivanov <roman.ivanov.7@gmail.com>
2021-06-28 09:09:35 +0200

    enable images cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -6,8 +6,8 @@
 docker rm spark-worker-1 spark-worker-2 spark-master spark-history jupyterlab sparklint
 
 # remove images and prune dangling
-#docker image rm cluster-base spark-base spark-master spark-worker spark-history jupyterlab sparklint
-#docker image prune -f
+docker image rm cluster-base spark-base spark-master spark-worker spark-history jupyterlab sparklint
+docker image prune -f
 
 # remove named volumes
 docker volume rm --force spark-optimisation-training_shared-workspace
