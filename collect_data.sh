f2af6fa4036c38323d3f812fd5eb0647d956f851
Roman Ivanov <roman.ivanov.7@gmail.com>
2021-06-28 11:31:11 +0200

    collecting data fixed

--- collect_data.sh
+++ collect_data.sh
@@ -1,31 +1,14 @@
 #!/bin/bash
 
-WITHCSV=0
 
-for arg in "$@"
-do 
-  case $arg in 
-     --with-csv)
-     WITHCSV=1
-     shift
-     ;;
-  esac
-done
+if [ ! -f shared-vol/data/meteo-data/parquet.tgz  ]; then
 
-if [ ! -f data/meteo-data/parquet.zip  ]; then
+  wget -P shared-vol/data/meteo-data -q https://storage.googleapis.com/spark-weather-data/flag_description.csv
+  wget -P shared-vol/data/meteo-data https://storage.googleapis.com/spark-weather-data/observation_type.csv
+  wget -P shared-vol/data/meteo-data https://storage.googleapis.com/spark-weather-data/stations.csv
+  wget -P shared-vol/data/meteo-data https://storage.googleapis.com/spark-weather-data/parquet.tgz
 
-  wget -P data/meteo-data -q https://storage.googleapis.com/spark-weather-data/flag_description.csv
-  wget -P data/meteo-data https://storage.googleapis.com/spark-weather-data/observation_type.csv
-  wget -P data/meteo-data https://storage.googleapis.com/spark-weather-data/stations.csv
-#  wget -P data/meteo-data https://storage.googleapis.com/meteo-data/parquet.zip
-
-  unzip data/meteo-data/parquet.zip -d data/meteo-data
-  rm -rf data/meteo-data/parquet.zip
-
-  if [ $WITHCSV == 1 ]; then
-    wget -P data/meteo-data https://meteo-data.s3-eu-west-1.amazonaws.com/meteo-data/csv.zip
-    unzip data/meteo-data/csv.zip -d data/meteo-data
-    rm -rf data/meteo-data/csv.zip    
-  fi
+  tar -zxf shared-vol/data/meteo-data/parquet.tgz --directory shared-vol/data/meteo-data
+#  rm -rf shared-vol/data/meteo-data/parquet.tgz
 
 fi
